1b8b0708f22e98ae88cc752a108c2799768b203d
user <bhaskermanas73@gmail.com>
2026-02-10 11:12:22 +0530

    js

---
+++ Day-11/script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("eventForm");
+const addBtn = document.getElementById("add-btn");
+
+let sampleEvent = [
+  {
+    eventName: "Hackathon",
+    eventDate: "2026-02-18",
+    eventDescription: "Hack KRMU 2026",
+    eventPriority: "High",
+  },
+  {
+    eventName: "Trip",
+    eventDate: "2026-02-28",
+    eventDescription: "Road trip",
+    eventPriority: "Low",
+  },
+];
