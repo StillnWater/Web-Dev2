1422f43b028069a18416f8fe0d57ad8959254d02
Manas Bhasker <bhaskermanas73@gmail.com>
2026-03-24 14:21:36 +0530

    updates

---
+++ React_Playground/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Navbar from './Navbar'
+import './App.css'
+const App = () => {
+  return (
+    <><Navbar />
+    <div>this is a simple React app</div>
+    </>
+  )
+}
+
+export default App  
+
+
---
+++ React_Playground/src/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/services">Services</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
